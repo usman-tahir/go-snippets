79c2de70e6d4f2b8e9bc21ab6ae45894ab734f28
Usman Tahir <tahir.usman.ali94@outlook.com>
2016-10-23 19:37:12 -0400

    added a simple numerical and string example

---
+++ strings.go
@@ -0,0 +1,9 @@
+
+package main
+import "fmt"
+
+func main() {
+  fmt.Println(len("Hello World")) // The length of the string
+  fmt.Println("Hello World"[0]) // The first letter (byte/ASCII representation)
+  fmt.Println("Hello," + " World") // String concatenation
+}
