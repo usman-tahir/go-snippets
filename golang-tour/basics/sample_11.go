ea47731192d7211862d5d17499790aae152cab04
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-01-08 10:17:03 -0500

    added the 'basics' directory from the previous example work through

---
+++ golang-tour/basics/sample_11.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Initialized with 'zero' values
+	var i int // 0
+	var f float64 // 0
+	var b bool // false
+	var s string // ""
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
